319639a45933280b3937ea3faa7bd5b30438fcca
mei.wang01 <mei.wang01@mljr.com>
2018-11-06 15:34:57 +0800

    提交 design pattern代码

---
+++ src/main/java/behavior/ChainOfResponsibility/ErrorLogger.java
@@ -0,0 +1,16 @@
+package behavior.ChainOfResponsibility;
+
+/**
+ * Created by wm on 2018/11/6.
+ */
+public class ErrorLogger  extends AbstractLogger {
+
+    public ErrorLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
---
+++ src/main/java/behavior/ChainOfResponsibility/FileLogger.java
@@ -0,0 +1,16 @@
+package behavior.ChainOfResponsibility;
+
+/**
+ * Created by wm on 2018/11/6.
+ */
+public class FileLogger  extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
