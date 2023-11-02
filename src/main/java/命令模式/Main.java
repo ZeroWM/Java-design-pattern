06ecb4ba0d40e3c4ea592262496fb6eed51f78bb
ZeroWM <wm4968@gmail.com>
2023-11-02 13:55:53 +0800

    feat: add command pattern

---
+++ src/main/java/命令模式/Main.java
@@ -0,0 +1,12 @@
+package 命令模式;
+
+public class Main {
+  public static void main(String[] args) {
+    Receiver r = new Receiver();
+    Command command = new ConcreteCommand(r);
+    Invoke invoke = new Invoke();
+
+    invoke.setCommand(command);
+    invoke.execute();
+  }
+}
