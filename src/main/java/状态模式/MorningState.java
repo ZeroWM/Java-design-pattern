270190756824ec70d9cdfd8d866240b88fbcb3ae
ZeroWM <wm4968@gmail.com>
2023-11-02 16:52:58 +0800

    feat: add state pattern

---
+++ src/main/java/状态模式/MorningState.java
@@ -0,0 +1,13 @@
+package 状态模式;
+
+public class MorningState extends State {
+  @Override
+  public void writeCode(Work work) {
+    if(work.getHour() < 12){
+      System.out.println("早上好");
+    }else {
+      work.setState(new NoonState());
+      work.writeCode();
+    }
+  }
+}
---
+++ src/main/java/状态模式/NoonState.java
@@ -0,0 +1,13 @@
+package 状态模式;
+
+public class NoonState extends State {
+  @Override
+  public void writeCode(Work work) {
+    if(work.getHour() < 13){
+      System.out.println("qia 午饭");
+    }else {
+      work.setState(new AfterNoonState());
+      work.writeCode();
+    }
+  }
+}
